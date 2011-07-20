7cd21f579427ccfe448db11a9f9a6599bef21180
Yu Feng <rainwoodman@gmail.com>
2011-07-20 18:47:39 -0400

    prefer parenthesis to {}.

--- support/messages.h
+++ support/messages.h
@@ -1,15 +1,15 @@
-#define MESSAGE(fmt,...) { fprintf(stderr, "MM:" fmt " %s:%d\n", \
-						## __VA_ARGS__, __FILE__, __LINE__); fflush(stderr); }
+#define MESSAGE(fmt,...) ( fprintf(stderr, "MM:" fmt " @(%s:%d)\n", \
+						## __VA_ARGS__, __FILE__, __LINE__), fflush(stderr) )
 
-#define WARNING(fmt,...) { fprintf(stderr, "WW:" fmt " %s:%d\n", \
-						## __VA_ARGS__, __FILE__, __LINE__); fflush(stderr); }
+#define WARNING(fmt,...) ( fprintf(stderr, "WW:" fmt " @(%s:%d)\n", \
+						## __VA_ARGS__, __FILE__, __LINE__), fflush(stderr) )
+
+#define LOG_ERROR(fmt,...) ( fprintf(stderr, "EE:" fmt " @(%s:%d)\n",\
+				 ## __VA_ARGS__, __FILE__, __LINE__), fflush(stderr))
+
 #ifndef MPI_INCLUDED
-#define ERROR(fmt,...) { fprintf(stderr, "EE:" fmt " %s:%d\n",\
-					 ## __VA_ARGS__, __FILE__, __LINE__); fflush(stderr); abort();}
+#define ERROR(fmt,...) ( LOG_ERROR(fmt, ## __VA_ARGS__ ), abort())
 #else
-#define ERROR(fmt,...) { fprintf(stderr, "EE:" fmt " %s:%d\n",\
-					 ## __VA_ARGS__, __FILE__, __LINE__); fflush(stderr); MPI_Abort(MPI_COMM_WORLD, -1);}
+#define ERROR(fmt,...) ( LOG_ERROR(fmt, ## __VA_ARGS__ ), MPI_Abort(MPI_COMM_WORLD, -1))
 #endif
-#define ERROR_R(code, fmt,...) { fprintf(stderr, "EE:" fmt " %s:%d\n",\
-					 ## __VA_ARGS__, __FILE__, __LINE__); fflush(stderr); return(code);}
 
