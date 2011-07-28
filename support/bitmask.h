f6ec3ea9bf87f03efbd687c97add620e26310b3b
Yu Feng <rainwoodman@gmail.com>
2011-07-28 14:32:46 -0400

    use unsigned integer for the bit ops
    (as they directly map to intel instructions)

--- support/bitmask.h
+++ support/bitmask.h
@@ -1,64 +1,80 @@
+#define BLOCK_TYPE unsigned int
 
-static inline char * bitmask_alloc(size_t size) {
-	char * rt = malloc(((size + 7) >> 3) + sizeof(size_t));
+#define BLOCK (sizeof(BLOCK_TYPE) * 8)
+static int __bitmask_log2[] = {
+// 0 , 1, 2, 3,  4,  5,  6,  7, 8, 9, 10, ...
+   -1, 3, 4, -1, 5, -1, -1, -1, 6, -1, -1, 
+};
+
+#define LOG2_BLOCK  (__bitmask_log2[sizeof(BLOCK_TYPE)])
+#define BLOCK_1 (BLOCK - 1)
+
+static inline BLOCK_TYPE * bitmask_alloc(size_t size) {
+	size_t total = (size + BLOCK_1 ) >> LOG2_BLOCK;
+	BLOCK_TYPE * rt = malloc(total * sizeof(BLOCK_TYPE) + sizeof(size_t));
 	*(size_t *) rt = size;
 	return rt;
 }
-static inline void bitmask_set_all(char * mask) {
+static inline void bitmask_set_all(BLOCK_TYPE * mask) {
 	size_t size = *(size_t *) mask;
-	size_t total = (size + 7 ) >> 3;
-	char * buf = mask + sizeof(size_t);
-	memset(buf, -1, total);
-	unsigned int m = ((1 << ((size & 7))) -1);
+	size_t total = (size + BLOCK_1 ) >> LOG2_BLOCK;
+	BLOCK_TYPE * buf = (BLOCK_TYPE*)(((size_t *) mask) + 1);
+	memset(buf, -1, total * sizeof(BLOCK_TYPE));
+	BLOCK_TYPE m = ((1 << ((size & BLOCK_1))) -1);
 	buf[total - 1] &= m;
 }
 
-static inline void bitmask_clear_all(char * mask) {
+static inline void bitmask_clear_all(BLOCK_TYPE * mask) {
 	size_t size = *(size_t *) mask;
-	size_t total = (size + 7 ) >> 3;
-	unsigned char * buf = mask + sizeof(size_t);
-	memset(buf, 0, total);
+	size_t total = (size + BLOCK_1 ) >> LOG2_BLOCK;
+	BLOCK_TYPE * buf = (BLOCK_TYPE*)(((size_t *) mask) + 1);
+	memset(buf, 0, total * sizeof(BLOCK_TYPE));
 }
 
-static inline void bitmask_set(char * mask, intptr_t idx) {
-	unsigned char * buf = mask + sizeof(size_t);
-	int offset = idx & 7;
-	unsigned char bit = 1 << offset;
-	//buf[idx >> 3] |= bit;
-	__sync_or_and_fetch(&buf[idx >> 3], bit);
+static inline void bitmask_set(BLOCK_TYPE * mask, intptr_t idx) {
+	BLOCK_TYPE * buf = (BLOCK_TYPE*)(((size_t *) mask) + 1);
+	int offset = idx & BLOCK_1;
+	BLOCK_TYPE bit = 1 << offset;
+	//buf[idx >> LOG2_BLOCK] |= bit;
+	__sync_or_and_fetch(&buf[idx >> LOG2_BLOCK], bit);
 }
 
-static inline void bitmask_clear(char * mask, intptr_t idx) {
-	unsigned char * buf = mask + sizeof(size_t);
-	int offset = idx & 7;
-	unsigned char bit = 1 << offset;
-	//buf[idx >> 3] &= ~bit;
-	__sync_and_and_fetch(&buf[idx >> 3], ~bit);
+static inline void bitmask_clear(BLOCK_TYPE * mask, intptr_t idx) {
+	BLOCK_TYPE * buf = (BLOCK_TYPE*)(((size_t *) mask) + 1);
+	int offset = idx & BLOCK_1;
+	BLOCK_TYPE bit = 1 << offset;
+	//buf[idx >> LOG2_BLOCK] &= ~bit;
+	__sync_and_and_fetch(&buf[idx >> LOG2_BLOCK], ~bit);
 }
 
-static inline int bitmask_test_and_clear(char * mask, intptr_t idx) {
-	unsigned char * buf = mask + sizeof(size_t);
-	int offset = idx & 7;
-	unsigned char bit = 1 << offset;
-//	return (buf[idx >> 3] & bit) != 0;
-	return __sync_fetch_and_and(&buf[idx >> 3], ~bit) & bit;
+static inline int bitmask_test_and_clear(BLOCK_TYPE * mask, intptr_t idx) {
+	BLOCK_TYPE * buf = (BLOCK_TYPE*)(((size_t *) mask) + 1);
+	int offset = idx & BLOCK_1;
+	BLOCK_TYPE bit = 1 << offset;
+//	return (buf[idx >> LOG2_BLOCK] & bit) != 0;
+	return __sync_fetch_and_and(&buf[idx >> LOG2_BLOCK], ~bit) & bit;
 }
 
-static inline int bitmask_test(char * mask, intptr_t idx) {
-	unsigned char * buf = mask + sizeof(size_t);
-	int offset = idx & 7;
-	unsigned char bit = 1 << offset;
-	return (buf[idx >> 3] & bit) != 0;
+static inline int bitmask_test(BLOCK_TYPE * mask, intptr_t idx) {
+	BLOCK_TYPE * buf = (BLOCK_TYPE*)(((size_t *) mask) + 1);
+	int offset = idx & BLOCK_1;
+	BLOCK_TYPE bit = 1 << offset;
+	return (buf[idx >> LOG2_BLOCK] & bit) != 0;
 }
 
-static inline size_t bitmask_sum(char * mask) {
-	int __builtin_popcount(unsigned int x);
-	char * buf = mask + sizeof(size_t);
+static inline size_t bitmask_sum(BLOCK_TYPE * mask) {
+//int __builtin_popcount(unsigned int x);
+	BLOCK_TYPE * buf = (BLOCK_TYPE*)(((size_t *) mask) + 1);
 	size_t size = *(size_t *) mask;
 	size_t sum = 0;
 	intptr_t i;
-	for(i = 0; i < (size + 7)>> 3; i++) {
-		sum += __builtin_popcount((unsigned char) buf[i]);
+	for(i = 0; i < (size + BLOCK_1)>> LOG2_BLOCK; i++) {
+		sum += __builtin_popcount((BLOCK_TYPE) buf[i]);
 	}
 	return sum;
 }
+#undef BLOCK_TYPE
+#undef LOG2_BLOCK
+#undef BLOCK
+#undef BLOCK_1
+
