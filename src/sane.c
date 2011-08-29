637788e67e1697a520f19dc1e26d9387ec765ed6
Yu Feng <rainwoodman@gmail.com>
2011-08-29 13:23:15 -0400

    testing blackbody spectra

--- src/sane.c
+++ src/sane.c
@@ -5,13 +5,16 @@
 #include "config.h"
 #include "psystem.h"
 
+extern void spec_dump(int spec);
 int main(int argc, char* argv[]) {
 	intptr_t i;
 	cfg_init(argv[1]);
-	cfg_dump_stream(stdout);
+//	cfg_dump_stream(stdout);
 
 	MESSAGE("HMF = %g", C_HMF);
 	MESSAGE("1 ryd verner cross section is %e", xs_get(XS_HI, 1.0));
+	MESSAGE("1 ryd energy = %e", U_RY_ENG);
+	MESSAGE("1 Kelvin energy = %e", C_BOLTZMANN * 1 * U_KELVIN);
 	MESSAGE("1e5K HI_CI %e HII_RC_A %e HII_RCC_A %e", 
 			ar_get(AR_HI_CI, 5),
 			ar_get(AR_HII_RC_A, 5),
@@ -25,9 +28,7 @@ int main(int argc, char* argv[]) {
 	MESSAGE("density 1 = %f proton / cm^3", 1 / units_parse(" mproton / cm cm cm"));
 	MESSAGE("--------unit sanity check------");
 
-	const int spec = spec_get("d6bh");
-	for(i = 0; i < 100; i++) {
-		printf("%lg\n", spec_gen_freq(spec));
-	}
+	const int spec = spec_get("sun");
+	spec_dump(spec);
 	return 0;
 }
