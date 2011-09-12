90e1bc8f7aa5a41afaadeec9e2bbad5d5009945a
Yu Feng <rainwoodman@gmail.com>
2011-09-12 13:59:24 -0400

    minor changes in solve.c

--- src/solve.c
+++ src/solve.c
@@ -64,7 +64,6 @@ int step_evolve_numerical (double Gamma, double gamma, double alpha, double y, d
 	const double chi = ar_get(AR_E_COMPC, logT) ; \
 \
 \
-	const int sign = xHI >= 1.0? -1: 1;
 
 static int function(double t, const double x[], double dxdt[], Step * step){
 
@@ -72,11 +71,11 @@ static int function(double t, const double x[], double dxdt[], Step * step){
 
 /* from GABE's notes  */
 	if(!CFG_ON_THE_SPOT) {
-		dxdt[1] = sign * seconds * alpha_AB * ye * xHII;
-		dxdt[0] = sign * seconds * (-yGdep_mean - gamma * ye * xHI + alpha_A * ye * xHII) / fac;
+		dxdt[0] = seconds * (-yGdep_mean - gamma * ye * xHI + alpha_A * ye * xHII) / fac;
+		dxdt[1] = seconds * alpha_AB * ye * xHII;
 	} else {
+		dxdt[0] = seconds * (-yGdep_mean - gamma * ye * xHI + alpha_B * ye * xHII) / fac;
 		dxdt[1] = 0;
-		dxdt[0] = sign * seconds * (-yGdep_mean - gamma * ye * xHI + alpha_B * ye * xHII) / fac;
 	}
 	if(CFG_ADIABATIC | CFG_ISOTHERMAL) {
 		dxdt[2] = 0.0;
@@ -86,9 +85,15 @@ static int function(double t, const double x[], double dxdt[], Step * step){
 			 (eta_HII * ye * xHII) +
 			 beta * ye * xHII + chi * ye);
 	//	MESSAGE("T = %g H=%g L=%g eta=%g psi=%g zeta=%g beta=%g xHI=%g\n", T, heat_mean, L, eta_HII, psi_HI, zeta_HI, beta, xHI);
-		dxdt[2] = sign * seconds * (heat_mean   - L);
+		dxdt[2] = seconds * (heat_mean   - L);
 	}
-
+/*
+	if(dxdt[0] > 0.0 && (xHI <=0.0 || xHI >= 1.0)) {
+		dxdt[0] *= -1;
+		dxdt[1] *= -1;
+		dxdt[2] *= -1;
+	}
+*/
 	return GSL_SUCCESS;
 }
 
@@ -97,35 +102,34 @@ static int jacobian(double t, const double x[], double *dfdx, double dfdt[], Ste
 	const int D = 3;
 	FETCH_VARS;
 
+	double dxdt0 = 0.0;
 	if(!CFG_ON_THE_SPOT) {
-		const double dxdt0 = (-yGdep_mean - gamma * ye * xHI + alpha_A * ye * xHII) / fac;
-		dfdx[0 * D + 0] = sign * seconds * (
+		dxdt0 = (-yGdep_mean - gamma * ye * xHI + alpha_A * ye * xHII) / fac;
+		dfdx[0 * D + 0] = seconds * (
 			- 2 * (xHI - xHII) * dxdt0 
 			+ gamma * xHI 
 			- ((gamma + alpha_A) * ye + alpha_A * xHII)
 			);
+		dfdx[1 * D + 0] = seconds * (-fac * alpha_AB * (ye + xHII));
 	} else {
-		const double dxdt0 = (-yGdep_mean - gamma * ye * xHI + alpha_B * ye * xHII) / fac;
-		dfdx[0 * D + 0] = sign * seconds * (
+		dxdt0 = (-yGdep_mean - gamma * ye * xHI + alpha_B * ye * xHII) / fac;
+		dfdx[0 * D + 0] = seconds * (
 			- 2 * (xHI - xHII) * dxdt0 
 			+ gamma * xHI 
 			- ((gamma + alpha_B) * ye + alpha_B * xHII)
 			);
+		dfdx[1 * D + 0] = 0;
 
 	}
 	dfdx[0 * D + 1] = 0;
 	dfdx[0 * D + 2] = 0;
-	if(!CFG_ON_THE_SPOT) {
-		dfdx[1 * D + 0] = 0;
-	} else {
-		dfdx[1 * D + 0] = sign * seconds * (-fac * alpha_AB * (ye + xHII));
-	}
+
 	dfdx[1 * D + 1] = 0;
 	dfdx[1 * D + 2] = 0;
 	if(CFG_ADIABATIC | CFG_ISOTHERMAL) {
 		dfdx[2 * D + 0] = 0;
 	} else {
-		dfdx[2 * D + 0] = sign * seconds * U_ERG * C_H_PER_MASS * fac * (
+		dfdx[2 * D + 0] = seconds * U_ERG * C_H_PER_MASS * fac * (
 			+ (eta_HII + beta) * (ye  + xHII)
 			+ (zeta_HI + psi_HI) * (xHI - ye)
 			+ chi
@@ -134,6 +138,13 @@ static int jacobian(double t, const double x[], double *dfdx, double dfdt[], Ste
 	dfdx[2 * D + 1] = 0;
 	dfdx[2 * D + 2] = 0;
 
+/*
+	if(dxdt0 > 0.0 && (xHI <=0.0 || xHI >= 1.0)) {
+		dfdx[0 * D + 0] *= -1;
+		dfdx[1 * D + 0] *= -1;
+		dfdx[2 * D + 0] *= -1;
+	}
+*/
 	dfdt[0] = 0;
 	dfdt[1] = 0;
 	dfdt[2] = 0;
