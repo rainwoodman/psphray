35159059663c0cee277300cfdc8b6e2f0966fa1e
Yu Feng <rainwoodman@gmail.com>
2011-09-21 16:16:50 -0400

    trivial test case changes.

--- src/test-bitmask.c
+++ src/test-bitmask.c
@@ -3,32 +3,48 @@
 #include <math.h>
 #include <stdlib.h>
 #include <string.h>
+#define BITMASK_SHUFFLE
 #include <bitmask.h>
 
 
+int * buffer;
+bitmask_t * mask;
 int main(int argc, char* argv[]) {
-#define LEN 127
-	char * mask = bitmask_alloc(LEN);
+#define LEN (65543)
+#define NT (1024)
+	buffer = calloc(sizeof(int), LEN);
+	mask = bitmask_alloc(LEN);
 	bitmask_clear_all(mask);
-	int i, j;
-	//printf("%p\n", mask);
-	for(i = LEN - 1; i >= 0; i--) {
-		if(bitmask_test_and_set(mask, i)) {
-			abort();
-		}
+	int i;
+	int js[NT];
+	#pragma omp parallel for private(i)
+	for(i = 0; i < NT; i++) {
+		int j;
 		for(j = 0; j < LEN; j++) {
-			printf("test bit %d against %d\n", i, j);
-			if(i!=j && bitmask_test(mask, j)) {
-				abort();
+			js[i] = j;
+			while(bitmask_test_and_set(mask, j)) {
+				continue;
 			}
-			if(i==j && !bitmask_test(mask, j)) {
-				abort();
-			}
+			buffer[j]++;
+			buffer[j]++;
+			buffer[j]--;
+			bitmask_clear(mask, j);
 		}
-		printf("test bit %d\n", i);
-		if(!bitmask_test_and_clear(mask, i)) {
+	}
+	for(i = 0; i < LEN; i++) {
+		if(buffer[i] != NT) {
+			printf("failed on %d\n", i);
 			abort();
 		}
 	}
-
+	intptr_t tests[] = {
+28250585,
+28250789,
+	};
+	for(i = 0; i < sizeof(tests) / sizeof(intptr_t); i++) {
+		intptr_t idx = bitmask_shuffle(tests[i]);
+		printf("%lX %lX %lX ", tests[i], idx, bitmask_shuffle(idx));
+		printf("ele= %ld", idx >> 5);
+		printf("offset = %d\n", idx & 31);
+	}
 }
--- src/test-solve.c
+++ src/test-solve.c
@@ -14,22 +14,38 @@ extern int step_solve(Step * step);
 int main(int argc, char* argv[]) {
 	cfg_init("../test/configfile");
 
+	CFG_ISOTHERMAL=1;
+	CFG_ADIABATIC=0;
+	CFG_ON_THE_SPOT=1;
+	double RC, CI;
 	printf("1e4K, ie= %g, check=%g\n", Tye2ie(1e4, 0), ieye2T(Tye2ie(1e4, 0), 0));
+	printf("1e4K, RC_A = %g RC_B = %g, CI = %g\n", 
+		RC=ar_get(AR_HII_RC_A, log10(1e4)), 
+		ar_get(AR_HII_RC_B, log10(1e4)),
+		CI=ar_get(AR_HI_CI, log10(1e4))
+	);
+	double P, Q, R;
+	P = RC;
+	Q = -(CI + 2 * RC);
+	R = CI + RC;
+	double q = -0.5 * (Q + copysign(sqrt(Q*Q - 4 * R*P), Q));
+	
+	printf("analytical limite=%g\n", P / q);
+		
 	Step step = {0};
-step.T = 99.8801; 
-step.lambdaHI=1.56959; 
-step.yeMET=0;
-step.nH=0.00105194;
-step.yGdep=3.14168e-05;
-step.ie=1.23988;
-step.heat=0.0949881;
 
-	double time = 0.05 * U_MYR;
+step.T = 1e4 + 100;
+step.lambdaH = 1.569;
+step.nH = 1e-3;
+step.yeMET=0;
+step.ie = 123;
+
+	double time = 5000.0 * U_MYR;
 	intptr_t i;
 	for(i = 0; i < 10; i++) {
-		double xHI = 0, xHII = 0;
-		lambdaHI_to_xHI_xHII(step.lambdaHI, xHI, xHII);
-		printf("%g %g %g %g %g %g \n", i * time,  step.ie, step.lambdaHI, xHI, xHII, step.dyGrec);
+		const double xHI = lambdaH_to_xHI(step.lambdaH);
+		const double xHII = lambdaH_to_xHII(step.lambdaH);
+		printf("%g %g %g %g %g %g \n", i * time,  step.ie, step.lambdaH, xHI, xHII, step.dyGrec);
 
 		step.heat = 0.0;
 		step.dyGrec = 0.0;
