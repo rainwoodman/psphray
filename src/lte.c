3c7afda99d6fecce70a9a030a5bcf2e2cd432245
Yu Feng <rainwoodman@gmail.com>
2011-09-25 22:26:51 -0400

    save the frequency in LTE.c

--- src/lte.c
+++ src/lte.c
@@ -22,6 +22,7 @@ struct {
 	double logT_max;
 	double logT_step;
 	double freq_step;
+	ARRAY_DEFINE_S(freq, double);
 } lte[3];
 
 static void lte_fill(int id, double freq_th, double freq_max, int xsid);
@@ -34,7 +35,7 @@ double lte_gen_freq(const int id, const double logT) {
 	int n = (logT - lte[id].logT_min) / lte[id].logT_step;
 	if(n < 0) n = 0;
 	if(n >= lte[id].NlogT) n = lte[id].NlogT - 1;
-	return gsl_ran_discrete(RNG, lte[id].dist[n]);
+	return lte[id].freq[gsl_ran_discrete(RNG, lte[id].dist[n])];
 }
 
 static void lte_fill(int id, double freq_th, double freq_max, int xsid) {
@@ -47,14 +48,19 @@ static void lte_fill(int id, double freq_th, double freq_max, int xsid) {
 	lte[id].freq_step = (freq_max - freq_th) / lte[id].Nfreq;
 	lte[id].logT_step = (lte[id].logT_max - lte[id].logT_min) / lte[id].NlogT;
 	ARRAY_RESIZE(lte[id].dist, gsl_ran_discrete_t *, lte[id].NlogT);
+	ARRAY_RESIZE(lte[id].freq, double, lte[id].Nfreq);
 	int i, j;
+	for(j = 0; j < lte[id].Nfreq; j++) {
+		double freq = lte[id].freq_min + lte[id].freq_step * j;
+		lte[id].freq[j] = freq;
+	}
 	for(i = 0; i < lte[id].NlogT; i++) {
 		double logT = lte[id].logT_min + lte[id].logT_step * i;
 		double T = exp(logT);
 		double f[lte[id].Nfreq];
 		double kT = C_BOLTZMANN * T;
 		for(j = 0; j < lte[id].Nfreq; j++) {
-			double freq = lte[id].freq_min + lte[id].freq_step * j;
+			double freq = lte[id].freq[j];
 			double sigma = xs_get(xsid, freq);
 			f[j] = sigma * freq * freq * freq * exp( - (freq - lte[id].freq_min) * U_RY_ENG / kT);
 		}
